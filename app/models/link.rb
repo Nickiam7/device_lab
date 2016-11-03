e9e329001f400fecc1b8eeced3fb52bfa57c8bb1
Nick McNeany <nmcneany@clm-nmcneany.enova.com>
2016-11-03 13:56:36 -0500

    pass last failing user-creates-links test

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  validates :url, presence: true
 end
--- spec/features/user_creates_link_spec.rb
+++ spec/features/user_creates_link_spec.rb
@@ -4,6 +4,29 @@ RSpec.feature "User creates a link", type: :feature do
 
 	scenario "Successfully" do
 		user = create(:user, id: 1)
+		link = create(:link)
+
+		visit new_user_session_path
+
+		sign_in(user.email, user.password)
+
+		expect(current_path).to eq(user_path(1))
+		expect(page).to have_content "Signed in successfully."
+		expect(page).to have_css("h2", text: "Hello #{user.username}")
+		click_link "Create new link"
+
+		expect(current_path).to eq(new_link_path)
+		expect(page).to have_css("h2", text: "Create a new link")
+		fill_in "Url", with: link.url
+		click_button "Send link to device lab"
+
+		expect(current_path).to eq(user_path(user))
+		expect(page).to have_content(link.url)
+	end
+
+	scenario "Unsuccessfully" do
+		user = create(:user, id: 1)
+		link = build(:link, url: "")
 
 		visit new_user_session_path
 
@@ -18,30 +41,7 @@ RSpec.feature "User creates a link", type: :feature do
 		expect(page).to have_css("h2", text: "Create a new link")
 		click_button "Send link to device lab"
 
-		link = create(:link)
-		expect(current_path).to eq(user_path(link.user_id))
-		expect(page).to have_content(link.url)
-	end
-
-	xit scenario "Unsuccessfully" do
-		user = create(:user, id: 1)
-
-		visit new_user_session_path
-
-		sign_in(user.email, user.password)
-
-		expect(current_path).to eq(user_path(1))
-		expect(page).to have_content "Signed in successfully."
-		expect(page).to have_css("h2", text: "Hello #{user.username}")
-		click_button "Create new link"
-
-		expect(current_path).to eq(new_link_path)
-		expect(page).to have_css("h2", text: "Create a new link")
-		click_button "Send link to device lab"
-
-		link = build(:link)
-
-		expect(current_path).to eq(new_link_path)
+		expect(current_path).to eq(links_path)
 	end
 
 	private
